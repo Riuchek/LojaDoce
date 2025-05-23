4bf587a4ad184982767fde1347c7d46a1612b029
Riuchek <riuchek@gmail.com>
2025-05-23 19:21:29 -0300

    feat: layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [],
+  modules: ['@nuxtjs/device'],
   app: {
     head: {
       title: 'Loja Doce',
